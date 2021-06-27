54a395bc8813305b9cd96707db11f0ec41471fae
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-27 15:38:01 -0500

    adds basic formatting

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,26 @@
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const day = props.date.getFullYear();
+
+
+    return (
+        <>
+            <div className="expense-item">
+            <div>{props.date.toISOString()}
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">{props.amount}</div>
+                </div>
+            </div>
+        </>
+        )
+}
+
+export default ExpenseItem;
