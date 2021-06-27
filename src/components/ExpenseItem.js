82cdc9115da29f5580fae7a4b45311350b35f95d
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-27 17:17:26 -0500

    adds card wrapper

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
-import "./ExpenseItem.css";
+import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 
@@ -6,13 +7,13 @@ const ExpenseItem = (props) => {
 
     return (
         <>
-            <div className="expense-item">
+            <Card className="expense-item">
                 <ExpenseDate date={props.date}/>
                 <div className="expense-item__description">
                     <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-            </div>
+            </Card>
         </>
     )
 }
