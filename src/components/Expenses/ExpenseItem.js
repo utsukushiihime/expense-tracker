f1cf6ab563e2c141ac5ef8afb694cbbd32e86fda
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-27 22:58:03 -0500

    cleanup unused code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,11 +7,6 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated Title");
-    console.log(title);
-  };
-
   return (
     <>
       <Card className="expense-item">
@@ -20,7 +15,6 @@ const ExpenseItem = (props) => {
           <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </>
   );
