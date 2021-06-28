4442767ea397e29c25e708b19e75880ec6050970
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-27 19:16:31 -0500

    adds prettier

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,28 @@
-import './ExpenseItem.css';
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
 
-    return (
-        <>
-            <Card className="expense-item">
-                <ExpenseDate date={props.date}/>
-                <div className="expense-item__description">
-                    <h2>{props.title}</h2>
-                    <div className="expense-item__price">${props.amount}</div>
-                </div>
-            </Card>
-        </>
-    )
-}
+  const clickHandler = () => {
+    title = "updated";
+  };
+
+  return (
+    <>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </>
+  );
+};
 
 export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,15 +1,14 @@
-import './Card.css';
+import React from "react";
+
+import "./Card.css";
 
 const Card = (props) => {
-    const classes = 'card ' + props.className;
+  const classes = "card " + props.className;
 
-    return(
-        <>
-            <div className={classes}>
-                {props.children}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={classes}>{props.children}</div>
+    </>
+  );
+};
 export default Card;
-
